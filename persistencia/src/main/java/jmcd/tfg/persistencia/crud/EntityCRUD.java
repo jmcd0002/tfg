5d24ae5024848e9ffa06400b9306005bd03428bd
jmcd0002 <jmcd0002@red.ujaen.es>
2019-03-07 20:09:46 +0100

    EntityCrud

--- persistencia/src/main/java/jmcd/tfg/persistencia/crud/EntityCRUD.java
+++ persistencia/src/main/java/jmcd/tfg/persistencia/crud/EntityCRUD.java
@@ -1,6 +1,9 @@
 package jmcd.tfg.persistencia.crud;
 
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import java.util.List;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
@@ -11,6 +14,49 @@ public abstract class EntityCRUD<T> {
     private static String persistencia = "persistencia";
 
     private static EntityManagerFactory entityManagerFactory;
+    protected Consumer persist = elemento -> dbTransactionalAction(EntityManager::persist, elemento);
+    protected Consumer borrado = id -> borrar(entityId -> dbTransactionalAction(EntityManager::remove, entityId), id);
+
+    protected static <S> void dbTransactionalAction(BiConsumer<EntityManager, S> action, S elemento) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        EntityTransaction tx = entityManager.getTransaction();
+        try {
+            tx.begin();
+            action.accept(entityManager, elemento);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            entityManager.close();
+        }
+    }
+
+    protected static <S> S dbConsult(Function<EntityManager, S> action) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        S solution = null;
+        try {
+            solution = action.apply(entityManager);
+        } finally {
+            entityManager.close();
+        }
+        return solution;
+    }
+
+    public static String getPersistencia() {
+        return persistencia;
+    }
+
+    public static void setPersistencia(String pers) {
+        persistencia = pers;
+    }
+
+    public static void initPersistencia() {
+        entityManagerFactory = Persistence.createEntityManagerFactory(persistencia);
+    }
+
+    public static void closePersistencia() {
+        entityManagerFactory.close();
+    }
 
     abstract T getEntidadPorId(Object id);
 
@@ -24,61 +70,14 @@ public abstract class EntityCRUD<T> {
 
     abstract boolean createOrUpdate(T elemento);
 
-    protected Consumer persist = elemento -> dbTransactionalAction(EntityManager::persist, elemento);
-    protected Consumer borrado = id -> borrar(entityId ->dbTransactionalAction(EntityManager::remove, entityId),id);
-
-    private void borrar(Consumer<Object> accion, Object entidadId){
-        if(exists(entidadId)){
+    private void borrar(Consumer<Object> accion, Object entidadId) {
+        if (exists(entidadId)) {
             accion.accept(entidadId);
         }
     }
 
     public boolean exists(Object id) {
         return getEntidadPorId(id) != null;
-    }
-
-    protected static <S> void dbTransactionalAction(BiConsumer<EntityManager, S> action, S elemento) {
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        EntityTransaction tx=entityManager.getTransaction();
-        try {
-            tx.begin();
-            action.accept(entityManager, elemento);
-            tx.commit();
-        }
-        catch (Exception e){
-            tx.rollback();
-        }
-        finally {
-            entityManager.close();
-        }
-    }
-
-    protected static <S> S dbConsult(Function<EntityManager, S> action) {
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
-        S solution=null;
-        try {
-            solution = action.apply(entityManager);
-        }
-        finally {
-            entityManager.close();
-        }
-        return solution;
-    }
-
-    public static void setPersistencia(String pers) {
-        persistencia = pers;
-    }
-
-    public static String getPersistencia() {
-        return persistencia;
-    }
-
-    public static void initPersistencia(){
-         entityManagerFactory = Persistence.createEntityManagerFactory(persistencia);
-    }
-
-    public static void closePersistencia(){
-        entityManagerFactory.close();
     }
 
 }
